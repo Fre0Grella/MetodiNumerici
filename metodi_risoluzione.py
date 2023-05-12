9128ba9dd0dab2d1322f02866340281dc45beb48
Marco Galeri <marco.galeri@studio.unibo.it>
2023-05-12 14:19:21 +0200

    Jacobi and sor added

--- metodi_risoluzione.py
+++ metodi_risoluzione.py
@@ -1,13 +1,49 @@
 import numpy as np
 
-def GaussSeidel(A,B,n):
-    #A: la matrice da risolvere
-    #B: il termine noto
-    #n: il numero di iterazioni
-    x = np.zeros(np.shape(B))
+def gauss_seidel(A,b,n=50,x=None):
+    """Solves the equation Ax=b via the Gauss-Seidel iterative method."""
+    # Create an initial guess if needed
+    if x is None:
+        x = np.zeros(len(A[0]))
+    
     L = np.tril(A)
     U = A-L
+
     for i in range(n):
-        x = np.dot(np.linalg.inv(L),B-np.dot(U,x))
+        x = np.dot(np.linalg.inv(L),b-np.dot(U,x))
     return x
 
+def jacobi(A,b,n=50,x=None):
+    """Solves the equation Ax=b via the Jacobi iterative method."""
+    # Create an initial guess if needed                                                                                                                                                            
+    if x is None:
+        x = np.zeros(len(A[0]))
+                                                                                                                                                                   
+    D = np.diag(A)
+    R = A - np.diagflat(D)
+                                                                                                                                                                         
+    for i in range(n):
+        x = (b - np.dot(R,x)) / D
+    return x
+
+def sor_solver(A, b, w, tollerance, x=None):
+    """Solves the equation Ax=b via Succesive Over Relaxation Method."""
+
+    """ w: relaxation factor (typically 0 < w < 2)
+        tollerance: the amount of error accepted
+    """
+    # Create an initial guess if needed                                                                                                                                                            
+    if x is None:
+        x = np.zeros(len(A[0]))
+
+    residual = np.linalg.norm(np.matmul(A, x) - b)
+    while residual > tollerance:
+        for i in range(A.shape[0]):
+            sigma = 0
+        for j in range(A.shape[1]):
+            if j != i:
+                sigma += A[i][j] * x[j]
+        x[i] = (1 - w) * x[i] + (w / A[i][i]) * (b[i] - sigma)
+        residual = np.linalg.norm(np.matmul(A, x) - b)
+        #print('Residual: {0:10.6g}'.format(residual))
+    return x
