790455a1b08296521db4ea0858f829f9e42646b0
Marco Galeri <marco.galeri@studio.unibo.it>
2023-05-15 14:41:43 +0200

    update

--- metodi_risoluzione.py
+++ metodi_risoluzione.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import lu
 
 def gauss_seidel(A,b,n,x=None):
     """Solves the equation Ax=b via the Gauss-Seidel iterative method."""
@@ -50,3 +51,29 @@ def sor_solver(A, b, w, tollerance, x=None):
         residual = np.linalg.norm(np.matmul(A, x) - b)
         #print('Residual: {0:10.6g}'.format(residual))
     return x
+
+def least_squares(A, b):
+    """Solves the equation Ax=b via Least Squares Method."""
+
+    n = A.shape[1]
+    r = np.linalg.matrix_rank(A)
+    U, sigma, VT = np.linalg.svd(A, full_matrices=True)
+    D_plus = np.diag(np.hstack([1/sigma[:r], np.zeros(n-r)]))
+    V = VT.T
+    A_plus = V.dot(D_plus).dot(U.T)
+    w = A_plus.dot(b)
+    error = np.linalg.norm(A.dot(w) - b, ord=2) ** 2
+    return w,error
+
+def LU(A):
+    PT,L,U=lu(A)  #Restituisce in output la trasposta della matrice di Permutazione
+    P=PT.T.copy()   #P è la matrice di permutazione
+    print("A=",A)
+    print("L=",L)
+    print("U=",U)
+    print("P=",P)
+    #LU è la fattorizzazione di P*A (terorema 2)
+    A2=P@A # equivale al prodotto matrice x matrice np.dot(P,A)
+    A2Fatt=L@U # equivale a np.dot(L,U)
+    print("Matrice P*A \n", A2)
+    print("Matrice ottenuta moltipicando Le ed U \n",A2Fatt)
